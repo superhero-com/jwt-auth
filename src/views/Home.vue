4757c2f708060ff3a52fa5ae76c89bb26b46811b
Denis Davidyuk <denis_davidyuk@hotmail.com>
2020-05-14 22:18:36 +1000

    Create sdk instance in joinMeeting

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -20,6 +20,29 @@ export default {
   data: () => ({ sdk: null }),
   methods: {
     async joinMeeting() {
+      const sdk = await RpcAepp({
+        name: 'JWT token generator',
+        compilerUrl: 'https://compiler.aepps.com',
+        nodes: [{
+          name: 'why should I have node defined if I\'m not using it?',
+          instance: await Node({ url: nodeUrl, internalUrl: nodeUrl }),
+        }],
+      });
+      const scannerConnection = await BrowserWindowMessageConnection({
+        connectionInfo: { id: 'spy' },
+      });
+      const detector = await Detector({ connection: scannerConnection });
+      await new Promise((resolve) => {
+        detector.scan(async ({ wallets, newWallet }) => {
+          const wallet = newWallet || Object.values(wallets)[0];
+          detector.stopScan();
+          await sdk.connectToWallet(await wallet.getConnection());
+          resolve();
+        });
+      });
+      await sdk.subscribeAddress('subscribe', 'current');
+      this.sdk = sdk;
+
       const message = `I would like to generate JWT token at ${new Date().toUTCString()}`;
       const signature = await this.sdk.signMessage(message);
       const address = await this.sdk.address();
@@ -30,34 +53,11 @@ export default {
         },
         body: JSON.stringify({ address, message, signature }),
       })).text();
-      window.location = `https://jitsi.z52da5wt.xyz/default?jwt=${token}`;
+      window.location = `https://jitsi.z52da5wt.xyz/asdf?jwt=${token}`;
     },
   },
   async created() {
-    const sdk = await RpcAepp({
-      name: 'JWT token generator',
-      compilerUrl: 'https://compiler.aepps.com',
-      nodes: [{
-        name: 'why should I have node defined if I\'m not using it?',
-        instance: await Node({ url: nodeUrl, internalUrl: nodeUrl }),
-      }],
-    });
-    const scannerConnection = await BrowserWindowMessageConnection({
-      connectionInfo: { id: 'spy' },
-    });
-    const detector = await Detector({ connection: scannerConnection });
-    await new Promise((resolve) => {
-      detector.scan(async ({ wallets, newWallet }) => {
-        const wallet = newWallet || Object.values(wallets)[0];
-        console.log(`Connecting to wallet ${wallet.name}`);
-        detector.stopScan();
-        await sdk.connectToWallet(await wallet.getConnection());
-        resolve();
-      });
-    });
-    await sdk.subscribeAddress('subscribe', 'current');
-    this.sdk = sdk;
-    window.sdk = sdk;
+    this.sdk = true;
   },
 };
 </script>
